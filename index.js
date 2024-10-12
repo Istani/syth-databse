22e41f58fffd9eafae2c7ccb56566bc583db2721
Sascha Kaufmann <Sascha.U.Kaufmann@googlemail.com>
2024-10-12 16:17:30 +0200

    feat: adding new tables for RPG

--- index.js
+++ index.js
@@ -17,3 +17,9 @@ exports.RPG_Inventories = require('./models/rpg_inventory.js');
 exports.RPG_Logs = require('./models/rpg_log.js');
 exports.RPG_Items = require('./models/rpg_items.js');
 exports.RPG_Story = require('./models/rpg_story.js');
+
+// Stream Details
+exports.User_Channels = require('./models/channel.js');
+exports.User_Chat_Servers = require("./models/chat_server.js");
+exports.User_Chat_Peoples = require("./models/chat_user.js");
+exports.User_Chat_Massages = require("./models/chat_message.js");
